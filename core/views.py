58d0aae711ab3d828f7a69060d2496a6e87c5937
Sol Aniel <solaaniel@gmail.com>
2026-08-06 20:50:03 -0300

    form

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def homepage(request):
     return render(request, 'core/homepage.html')
@@ -12,23 +13,34 @@ def login(request):
 def cadastro(request):
     return render(request, 'core/cadastro.html')
 
+@login_required
 def dashboard(request):
     return render(request, 'core/dashboard.html')
 
+@login_required
 def perfilCliente(request):
     return render(request, 'core/perfilCliente.html')
 
+@login_required
 def perfilVendedor(request):
     return render(request, 'core/perfilVendedor.html')
 
+@login_required
 def perfilAdmin(request):
     return render(request, 'core/perfilAdmin.html')
 
+@login_required
 def cardapio(request):
     return render(request, 'core/cardapio.html')
 
+@login_required
 def produto(request):
     return render(request, 'core/produto.html')
 
+@login_required
 def carrinho(request):
     return render(request, 'core/carrinho.html')
+
+@login_required
+def pedido(request):
+    return render(request, 'core/pedido.html')
