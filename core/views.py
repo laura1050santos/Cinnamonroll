2106a67c83dcc87fb2005d95c20bbd30d89624cd
sara martins <sarammartinsoliveira@gmail.com>
2026-08-06 19:29:54 -0300

    Primeiro teste

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+def inicio(request):
+    return HttpResponse("Olá! Esta é minha primeira página em Django.")
