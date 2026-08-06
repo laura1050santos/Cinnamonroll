56ba090dc9d2e4cff767f5d390c58186cb860b4d
Sol Aniel <solaaniel@gmail.com>
2026-08-06 20:18:24 -0300

    Pedido e Static

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
-from .views import inicio
+from .views import *
 
 urlpatterns = [
-path('', homepage, name='homepage'),
-path('searchpage/', searchpage, name='searchpage'),
-path('login/', login, name='login'),
-path('cadastro/', cadastro, name='cadastro'),
-path('dashboard/', dashboard, name='dashboard'),
-path('perfilCliente/', perfilCliente, name='perfilCliente'),
-path('perfilVendedor/', perfilVendedor, name='perfilVendedor'),
-path('perfilAdmin/', perfilAdmin, name='perfilAdmin'),
-path('cardapio/', cardapio, name='cardapio'),
-path('produto/', produto, name='produto'),
-path('carrinho/', carrinho, name='carrinho'),
+    path('', homepage, name='homepage'),
+    path('searchpage/', searchpage, name='searchpage'),
+    path('login/', login, name='login'),
+    path('cadastro/', cadastro, name='cadastro'),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('perfilCliente/', perfilCliente, name='perfilCliente'),
+    path('perfilVendedor/', perfilVendedor, name='perfilVendedor'),
+    path('perfilAdmin/', perfilAdmin, name='perfilAdmin'),
+    path('cardapio/', cardapio, name='cardapio'),
+    path('produto/', produto, name='produto'),
+    path('carrinho/', carrinho, name='carrinho'),
+    path('pedido/', pedido, name='pedido')
 ]
