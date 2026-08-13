12b73adc3c70c01ab89be39bb690ce616486d7cc
Sol Aniel <solaaniel@gmail.com>
2026-08-13 20:50:08 -0300

    xvcxffgrd

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+
+cliente_group, _ = Group.objects.get_or_create(
+    name="Clientes"
+)
+
+vendedor_group, _ = Group.objects.get_or_create(
+    name="Vendedores"
+)
+
+admin_group, _ = Group.objects.get_or_create(
+    name="Administradores"
+)
