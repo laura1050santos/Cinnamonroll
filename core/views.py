ff854ff433f1d71c766265dc0f1219f5bd56a90f
sara <sarammartinsoliveira@gmail.com>
2026-09-03 13:47:56 -0300

    Pequenininho

--- core/views.py
+++ core/views.py
@@ -14,7 +14,16 @@ def homepage(request):
     return render(request, 'core/homepage.html', {'lojas': lojas})
 
 def searchpage(request):
-    return render(request, 'core/searchpage.html')
+    query = request.GET.get('q', '').strip()
+    produtos = []
+
+    if query:
+        produtos = Produto.objects.filter(nome__icontains=query)
+
+    return render(request, 'core/searchpage.html', {
+        'produtos': produtos,
+        'query': query
+    })
 
 def cadastro(request):
     if request.method == 'POST':
